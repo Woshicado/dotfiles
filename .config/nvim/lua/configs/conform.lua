3516d17b8cb7a6611803f59c7aefa3c91738a010
Joshua Meyer <woshicado@woshicado.eu>
2025-03-20 09:07:05 +0100

    Python ruff autofmt imports

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -3,11 +3,31 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    python = { "isort", "ruff_format" },
+    python = { "ruff_organize_imports", "ruff_format" },
     yaml = { "yamlfix" },
     json = { "jq" },
   },
-
+  formatters = {
+    ruff_organize_imports = {
+      command = "ruff",
+      args = {
+        "check",
+        "--force-exclude",
+        "--select=I001",
+        "--fix",
+        "--exit-zero",
+        "--stdin-filename",
+        "$FILENAME",
+        "-",
+      },
+      stdin = true,
+      cwd = require("conform.util").root_file {
+        "pyproject.toml",
+        "ruff.toml",
+        ".ruff.toml",
+      },
+    },
+  },
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
   --   timeout_ms = 500,
