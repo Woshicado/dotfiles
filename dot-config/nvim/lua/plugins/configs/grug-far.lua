baa71f6b1abbccc5bbd4991b8cee74673618b600
Joshua Meyer <woshicado@woshicado.eu>
2025-10-20 21:26:42 +0200

    GrugFar+Oil

--- dot-config/nvim/lua/plugins/configs/grug-far.lua
+++ dot-config/nvim/lua/plugins/configs/grug-far.lua
@@ -1,6 +1,7 @@
 return {
 	"MagicDuck/grug-far.nvim",
 	keys = { '<leader>fs' },
+  cmd = { "GrugFar" },
 	config = function()
 		require("grug-far").setup({})
 	end,
--- dot-config/nvim/lua/plugins/configs/oil.lua
+++ dot-config/nvim/lua/plugins/configs/oil.lua
@@ -15,15 +15,36 @@ return {
 				["<C-t>"] = { "actions.select", opts = { tab = true } },
 				["<C-p>"] = "actions.preview",
 				["<C-c>"] = { "actions.close", mode = "n" },
-				-- ["<C-l>"] = "actions.refresh",
+				["<leader>rr"] = "actions.refresh",
 				["-"] = { "actions.parent", mode = "n" },
 				["_"] = { "actions.open_cwd", mode = "n" },
 				["`"] = { "actions.cd", mode = "n" },
 				["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-				["gs"] = { "actions.change_sort", mode = "n" },
+				-- ["gs"] = { "actions.change_sort", mode = "n" },
 				["gx"] = "actions.open_external",
 				["g."] = { "actions.toggle_hidden", mode = "n" },
 				["g\\"] = { "actions.toggle_trash", mode = "n" },
+				["gs"] = { -- open GrugFar with the current directory as the search path
+					callback = function()
+						-- get the current directory
+						local prefills = { paths = require("oil").get_current_dir() }
+
+						local grug_far = require("grug-far")
+						-- instance check
+						if not grug_far.has_instance("explorer") then
+							grug_far.open({
+								instanceName = "explorer",
+								prefills = prefills,
+								staticTitle = "Find and Replace from Explorer",
+							})
+						else
+							grug_far.get_instance("explorer"):open()
+							-- updating the prefills without clearing the search and other fields
+							grug_far.get_instance("explorer"):update_input_values(prefills, false)
+						end
+					end,
+					desc = "oil: Search in directory",
+				},
 			},
 			columns = {
 				{ "permissions", highlight = "Special" },
